3e14c9c68dd9fb70dd4f5202d61045e4ecdb23ce
ikramgondal <56062316+ikramgondal@users.noreply.github.com>
2020-01-02 20:36:08 +0500

    not working

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-
+//
 import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
 import { MapModalComponent } from './map-modal/map-modal.component';
 import { ImagePickerComponent } from './pickers/image-picker/image-picker.component';
