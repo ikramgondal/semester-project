8437ce26045161a333ad3a536c8f6a64f46fc866
ikramgondal <56062316+ikramgondal@users.noreply.github.com>
2020-01-02 20:27:29 +0500

    Solved RunTime Problem

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,6 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 //Author: Ikram Gondal
+//Solved Solution
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
